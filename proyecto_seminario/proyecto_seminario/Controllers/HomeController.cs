f89e4d5a12d1b6d905411d29fa6fbed37e86025c
Pedro Alfonzo Condori Fernandez <sirius.black.canuto@gmail.com>
2012-05-27 22:12:21 -0600

    Principal

--- proyecto_seminario/proyecto_seminario/Controllers/HomeController.cs
+++ proyecto_seminario/proyecto_seminario/Controllers/HomeController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data;
+using System.Data.SqlClient;
+using proyecto_seminario.Models;
 namespace proyecto_seminario.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Pagina de inicio!";
-
+            SqlConnection con = new SqlConnection("Data Source=ILLUSION_V2;Initial Catalog=semi2;Integrated Security=True");
+            con.Open();
+            SqlCommand com = new SqlCommand("select detalle,ruta_imagen from libro", con);
+            SqlDataReader reader = com.ExecuteReader();
+            List<Libro> x= new List<Libro>();
+            while (reader.Read())
+            {
+                Libro l = new Libro();
+                l.ruta = reader.GetString(1);
+                l.detalle = reader.GetString(0);
+                x.Add(l);
+            }
+            ViewBag.valor = x;
             return View();
         }
 
---
+++ proyecto_seminario/proyecto_seminario/Models/Libro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proyecto_seminario.Models
+{
+    public class Libro
+    {
+        
+        public string detalle { set; get; }
+        public string ruta { set; get; }
+
+    }
+}
